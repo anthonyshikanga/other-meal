ec0d6e127760554386151ac6874db272fae54d63
Tony <anthonyshikanga@gmail.com>
2017-06-07 19:23:06 +0300

    Added a new file

---
+++ app/main.ts
@@ -0,0 +1,13 @@
+/*This file is responsible for loading the AppModule file*/
+
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+
+import { AppModule } from './app.module';
+
+
+
+const platform = platformBrowserDynamic();
+
+
+
+platform.bootstrapModule(AppModule);
